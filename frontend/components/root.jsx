67954d948a91c1e97b791eb0c715d83a460231fd
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2018-12-13 10:17:53 -0800

    Changed the case for App.jsx

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import App from './app.jsx';
+import App from './App.jsx';
 import { HashRouter } from 'react-router-dom';
 
 const Root = ({ store }) => (
