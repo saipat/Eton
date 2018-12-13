055699a4b599bb856fd9e2e02f4a027d6600e528
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2018-12-13 10:34:03 -0800

    Add seed to the seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+
+demo1 = User.create!(
+  email: 'demouser11@email',
+  password: '123456'
+)
