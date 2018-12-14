355a8c33bf16e053db6e10571ad58504fe9c9587
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2018-12-13 17:56:46 -0800

    add style, image and modify the sidebar components

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -21,10 +21,45 @@ class Sidebar extends React.Component {
             return null;
         }
         let notebooks = Object.values(this.props.notebooks);
+        let dropdown = document.getElementsByClassName("dropdown-btn");
+        let i;
+
+        for (i = 0; i < dropdown.length; i++) {
+            dropdown[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                let dropdownContent = this.nextElementSibling;
+                if (dropdownContent.style.display === "block") {
+                    dropdownContent.style.display = "none";
+                } else {
+                    dropdownContent.style.display = "block";
+                }
+            });
+        }
         return (
-            <div className="sidebar">
-                <h3>{this.state.currentUser.email}</h3>
+            <div className="grid-container">
+                <div className="sidebar">
+                    <div className="sd-title">
+                        <img src={window.sidebarLogoURL} alt="sidebar_logo" className="sidebar-logo" />
+                        <h3>{this.state.currentUser.email}</h3>
+                    </div>
+                    <form className="sd-search">
+                        <input type="search" placeholder="search all values" className="sd-search-input"></input>
+                        <button type="submit" id="search-icon"><i className="fa fa-search"></i></button>
+                    </form>
+                    <div>
+                        <button className="new-note"><i className="fa fa-plus"></i>New Note</button>
+                    </div>
+                    <button class="dropdown-btn">Dropdown
+                        <i class="fa fa-caret-down"></i>
+                    </button>
+                    <div class="dropdown-container">
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                    </div>
+                </div>
             </div>
+            
         )
     }
 
